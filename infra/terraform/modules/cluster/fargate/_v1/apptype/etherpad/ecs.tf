72ee20ff134dce43f5d8e5711134e6c6716d4022
KPH <whereiskurt@gmail.com>
2025-04-06 00:12:14 -0400

    -Fix hardcoded region

--- infra/terraform/modules/cluster/fargate/_v1/apptype/etherpad/ecs.tf
+++ infra/terraform/modules/cluster/fargate/_v1/apptype/etherpad/ecs.tf
@@ -58,7 +58,7 @@ resource "aws_ecs_task_definition" "service" {
         app : "http://${var.env_zonename}:9001" ##HTTP proxying from nginx
       },
 
-      p_email : "${local.parameter_arn}/use1.email.defcon.run",
+      p_email : "${local.parameter_arn}/${var.region_label}.email.defcon.run",
       p_app : "${local.parameter_arn}/${var.region_zonename}",
       p_cluster : "${local.parameter_arn}/${var.region_label}.defcon.run",
 
