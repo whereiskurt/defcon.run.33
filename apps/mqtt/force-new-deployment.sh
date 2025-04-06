0aed08d2f47e21a9f834b219b3d9fde38b0bf5b4
KPH <whereiskurt@gmail.com>
2025-04-06 01:24:21 -0400

    -Fix hardcoded region -Adding MQTT variable

--- apps/mqtt/force-new-deployment.sh
+++ apps/mqtt/force-new-deployment.sh
@@ -2,8 +2,8 @@ export PAGER=
 export AWS_PROFILE=${AWS_PROFILE:-application}
 export AWS_REGION=${AWS_REGION:-"us-east-1"}
 export CLUSTER=${CLUSTER:-"ecs-use1-defcon-run"}
-export MQTT="mqtt-defcon-run"
+export MQTT=${MQTT:-"mqtt-defcon-run"}
 
-LATEST_MQTT=$(aws ecs list-task-definitions --family-prefix mqtt-defcon-run --sort DESC --query "taskDefinitionArns[0]" --output text)
+LATEST_MQTT=$(aws ecs list-task-definitions --family-prefix $MQTT --sort DESC --query "taskDefinitionArns[0]" --output text)
 
 aws ecs update-service --cluster $CLUSTER --service $MQTT --force-new-deployment --task-definition $LATEST_MQTT
