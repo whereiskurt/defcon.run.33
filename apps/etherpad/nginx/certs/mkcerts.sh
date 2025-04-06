66b4246248333abee8a172bf1672c14449593308
KPH <whereiskurt@gmail.com>
2025-04-06 01:58:33 -0400

    -Fixing case for find/replace setup

--- apps/etherpad/nginx/certs/mkcerts.sh
+++ apps/etherpad/nginx/certs/mkcerts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Generate private key, self-signed certificate, and CSR
-openssl req -new -newkey rsa:2048 -nodes -keyout nginx-selfsigned.key -out nginx-selfsigned.csr -subj "/C=US/ST=Nevada/L=LasVegas/O=DEFCON.run/OU=Engineering/CN=etherpad.defcon.run"
+openssl req -new -newkey rsa:2048 -nodes -keyout nginx-selfsigned.key -out nginx-selfsigned.csr -subj "/C=US/ST=Nevada/L=LasVegas/O=defcon.run/OU=Engineering/CN=etherpad.defcon.run"
 
 # Create self-signed certificate from the private key
 openssl x509 -req -days 365 -in nginx-selfsigned.csr -signkey nginx-selfsigned.key -out nginx-selfsigned.crt
