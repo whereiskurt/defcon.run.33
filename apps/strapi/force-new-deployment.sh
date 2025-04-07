11cb37839ddf5cff02471be9b24f7a0edba2d6a9
KPH <whereiskurt@gmail.com>
2025-04-06 22:20:30 -0400

    -Adding $STRAPI

--- apps/strapi/force-new-deployment.sh
+++ apps/strapi/force-new-deployment.sh
@@ -2,8 +2,8 @@ export PAGER=
 export AWS_PROFILE=${AWS_PROFILE:-application}
 export AWS_REGION=${AWS_REGION:-"us-east-1"}
 export CLUSTER=${CLUSTER:-"ecs-use1-defcon-run"}
-export STRAPI="strapi-defcon-run"
+export STRAPI=${STRAPI:-"strapi-defcon-run"}
 
-LATEST_STRAPI=$(aws ecs list-task-definitions --family-prefix strapi-defcon-run --sort DESC --query "taskDefinitionArns[0]" --output text)
+LATEST_STRAPI=$(aws ecs list-task-definitions --family-prefix $STRAPI --sort DESC --query "taskDefinitionArns[0]" --output text)
 
 aws ecs update-service --cluster $CLUSTER --service $STRAPI --force-new-deployment --task-definition $LATEST_STRAPI
