fcab24ed3d2074cabe9fe3050b09b62e336d8923
KPH <whereiskurt@gmail.com>
2025-04-07 18:20:24 -0400

    -infra and app "site.tld" rename

--- apps/mqtt/mosquitto/deploy.mosquitto.sh
+++ apps/mqtt/mosquitto/deploy.mosquitto.sh
@@ -9,7 +9,7 @@ export REPO_NAME=${REPO_NAME:-"mosquitto.mqtt.defcon.run"}
 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -f defcon-run/Dockerfile -t $REPO_NAME:$IMAGE_TAG defcon-run/
+  -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
 
 aws ecr get-login-password --region ${AWS_REGION} \
   | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
