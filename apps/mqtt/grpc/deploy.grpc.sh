a1692831b3fd8f5758cf7337dfed34b0b922ac62
KPH <whereiskurt@gmail.com>
2025-04-21 17:35:43 -0400

    -Pulling latest meshtk from repo with protobuf setup

--- apps/mqtt/grpc/deploy.grpc.sh
+++ apps/mqtt/grpc/deploy.grpc.sh
@@ -7,18 +7,21 @@ export AWS_ACCOUNT_ID=${AWS_ACCOUNT_ID:-$(aws sts get-caller-identity --query "A
 export IMAGE_TAG=${IMAGE_TAG:-"v0.0.1"}
 export REPO_NAME=${REPO_NAME:-"grpc.mqtt.defcon.run"}
 
-# docker buildx build \
-#   --platform linux/amd64 \
-#   -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
+rm -fr ./site-tld/meshtk
+git clone https://github.com/whereiskurt/meshtk ./site-tld/meshtk
 
 docker buildx build \
-  --platform linux/amd64,linux/arm64 \
+  --platform linux/arm64 \
   -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
 
-aws ecr get-login-password --region ${AWS_REGION} \
-  | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+# docker buildx build \
+#   --platform linux/amd64,linux/arm64 \
+#   -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
 
-docker tag "${REPO_NAME}:${IMAGE_TAG}" \
-  "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+# aws ecr get-login-password --region ${AWS_REGION} \
+#   | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
-docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+# docker tag "${REPO_NAME}:${IMAGE_TAG}" \
+#   "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+
+# docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
