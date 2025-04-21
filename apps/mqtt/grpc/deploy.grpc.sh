b60f753f1d4a17847f511617684b27633bdff803
KPH <whereiskurt@gmail.com>
2025-04-21 18:49:18 -0400

    -Update deploy and acl

--- apps/mqtt/grpc/deploy.grpc.sh
+++ apps/mqtt/grpc/deploy.grpc.sh
@@ -10,18 +10,18 @@ export REPO_NAME=${REPO_NAME:-"grpc.mqtt.defcon.run"}
 rm -fr ./site-tld/meshtk
 git clone https://github.com/whereiskurt/meshtk ./site-tld/meshtk
 
-docker buildx build \
-  --platform linux/arm64 \
-  -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
-
 # docker buildx build \
-#   --platform linux/amd64,linux/arm64 \
+#   --platform linux/arm64 \
 #   -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
 
-# aws ecr get-login-password --region ${AWS_REGION} \
-#   | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
+docker buildx build \
+  --platform linux/amd64,linux/arm64 \
+  -f site-tld/Dockerfile -t $REPO_NAME:$IMAGE_TAG site-tld/
 
-# docker tag "${REPO_NAME}:${IMAGE_TAG}" \
-#   "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+aws ecr get-login-password --region ${AWS_REGION} \
+  | docker login --username AWS --password-stdin "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com"
 
-# docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+docker tag "${REPO_NAME}:${IMAGE_TAG}" \
+  "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
+
+docker push "${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${REPO_NAME}:${IMAGE_TAG}"
