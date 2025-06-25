abcf93678ccac38299a816c245aa0f0e64ddb678
KPH <whereiskurt@gmail.com>
2025-06-24 22:50:56 -0400

    -Add cloudfront record for route53

--- infra/terraform/modules/cluster/fargate/_v1/route53.tf
+++ infra/terraform/modules/cluster/fargate/_v1/route53.tf
@@ -7,7 +7,7 @@ data "aws_lb" "nlb" {
 resource "aws_route53_record" "app_record" {
   count   = var.is_primary_deployment && var.app_type == "mqtt" ? 1 : 0
   zone_id = var.zone_map[var.env_zonename].zone_id
-  name    = "${var.env_zonename}"
+  name    = var.env_zonename
   type    = "A"
 
   alias {
@@ -22,7 +22,7 @@ resource "aws_route53_record" "app_record" {
 resource "aws_route53_record" "app_region_record" {
   count   = var.is_primary_deployment && var.app_type == "mqtt" ? 1 : 0
   zone_id = var.zone_map[var.env_zonename].zone_id
-  name    = "${var.region_zonename}"
+  name    = var.region_zonename
   type    = "A"
 
   alias {
@@ -30,6 +30,22 @@ resource "aws_route53_record" "app_region_record" {
     zone_id                = data.aws_lb.nlb[0].zone_id
     evaluate_target_health = true
   }
+  allow_overwrite = true
+  provider        = aws.global-application
+}
+
+// TODO: Consider moving this into the apptype ! :) 
+resource "aws_route53_record" "nextjs_primary_record" {
+  count   = var.is_primary_deployment && var.app_type == "nextjs" ? 1 : 0
+  zone_id = var.zone_map[var.env_zonename].zone_id
+  name    = var.env_zonename
+  type    = "A"
+  alias {
+    name                   = aws_cloudfront_distribution.cf_public[0].domain_name
+    zone_id                = aws_cloudfront_distribution.cf_public[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+
   allow_overwrite = true
   provider        = aws.global-application
 }
