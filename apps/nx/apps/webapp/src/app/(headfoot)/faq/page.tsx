223e8a261461e0d27bb5fac396761c7dcdee2ef8
KPH <whereiskurt@gmail.com>
2025-07-01 20:41:57 -0400

    -Idiomatic page layout

--- apps/nx/apps/webapp/src/app/(headfoot)/faq/page.tsx
+++ apps/nx/apps/webapp/src/app/(headfoot)/faq/page.tsx
@@ -6,26 +6,28 @@ import { Heading, Lead } from '@components/ui/typography';
 
 // Dynamic imports for components
 const RichText = dynamic(() => import('@components/cms/rich'), { ssr: true });
-const FAQQuestions = dynamic(() => import('@components/cms/faq/questions'), { ssr: true });
+const Questions = dynamic(() => import('@components/cms/faq/questions'), { ssr: true });
 
 export default async function Welcome() {
 
   const raw = await strapi('/faq?populate=*');
 
-  const body = raw.data.page_body;
-  const summary = raw.data.page_summary;
-  const title = raw.data.page_title;
-  const bottom = raw.data.page_bottom;
-  const questions = raw.data.FAQ;
+  const { 
+    page_body, 
+    page_summary, 
+    page_title, 
+    page_bottom, 
+    FAQ: questions 
+  } = raw.data;
 
   const className = '';
   return (
     <>
-      <Heading>{title}</Heading>
-      <Lead>{summary}</Lead>
-      <RichText className="pt-4" body={body} />
-      <FAQQuestions className={className} questions={questions} />
-      <RichText className="pt-4" body={bottom} />
+      <Heading>{page_title}</Heading>
+      <Lead>{page_summary}</Lead>
+      <RichText className="pt-4" body={page_body} />
+      <Questions className={className} questions={questions} />
+      <RichText className="pt-4" body={page_bottom} />
     </>
   );
 }
