bb0b246f006250f73eaaf1d4ee52a45ae1ee5584
KPH <whereiskurt@gmail.com>
2025-07-18 02:14:11 -0400

    -Added dark maps

--- apps/nx/apps/webapp/src/app/(headfoot)/routes/page.tsx
+++ apps/nx/apps/webapp/src/app/(headfoot)/routes/page.tsx
@@ -2,6 +2,7 @@
 
 import { strapi } from '@components/cms/data';
 import ClientMap from '@components/map/ClientMap';
+import styles from './routes.module.css';
 
 export default async function Page() {
 
@@ -9,12 +10,13 @@ export default async function Page() {
   const mqtt_nodes = await livenodes("https://mqtt.defcon.run/map/nodes.json");
 
   return (
-    <div className={"mx-auto w-[98%] p-2"}>
-      <ClientMap raw={JSON.stringify(routes.data)} mqtt_nodes={JSON.stringify(mqtt_nodes)} center={[36.1320813, -115.1667648]} />
+    <div className={`mx-auto w-[98%] p-2 ${styles.routesContainer}`}>
+      <div className={styles.mapWrapper}>
+        <ClientMap raw={JSON.stringify(routes.data)} mqtt_nodes={JSON.stringify(mqtt_nodes)} center={[36.1320813, -115.1667648]} />
+      </div>
     </div>
   );
 } 
-
 
 async function livenodes(url: string) {
   const res = await fetch(`${url}`, {
