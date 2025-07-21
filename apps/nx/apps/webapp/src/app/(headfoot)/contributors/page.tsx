fb95988aae5abc72215b18cfc069470113d67661
KPH <whereiskurt@gmail.com>
2025-07-21 17:55:18 -0400

    -Basic contributor page and strapi data

---
+++ apps/nx/apps/webapp/src/app/(headfoot)/contributors/page.tsx
@@ -0,0 +1,23 @@
+'use server';
+
+import dynamic from 'next/dynamic';
+import { strapi } from '@components/cms/data';
+import { Heading, Lead } from '@components/text-effects/Common';
+
+// Dynamic imports for components
+const RichText = dynamic(() => import('@components/cms/rich'), { ssr: true });
+
+export default async function Contributors() {
+
+  const raw = await strapi('/contributor?populate=*');
+  const { page_body, page_summary, page_title, page_bottom } = raw.data;
+
+  return (
+    <>
+      <Heading>{page_title}</Heading>
+      <Lead>{page_summary}</Lead>
+      <RichText body={page_body} />
+      <RichText body={page_bottom} />
+    </>
+  );
+}
