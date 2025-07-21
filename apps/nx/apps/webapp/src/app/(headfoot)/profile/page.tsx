706ec2ce5251a02c70075f4e975444e022f0efed
KPH <whereiskurt@gmail.com>
2025-07-20 20:16:10 -0400

    -Using 'any' for discord and github profiles

--- apps/nx/apps/webapp/src/app/(headfoot)/profile/page.tsx
+++ apps/nx/apps/webapp/src/app/(headfoot)/profile/page.tsx
@@ -16,18 +16,16 @@ export default async function Page() {
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-2xl font-bold mb-6">Profile</h1>
       
-      <div className="mb-6">
+      <div className="">
         <UserDetails />
       </div>
       
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
         <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-4">Your MQTT Credentials</h2>
           <MqttCredentials />
         </div>
         
         <div className="mt-4">
-          <h2 className="text-xl font-semibold mb-4">Strava Integration</h2>
           <StravaConnection />
         </div>
       </div>
